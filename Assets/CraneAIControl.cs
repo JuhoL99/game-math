65eb3832f55083d18c1e2e19de272bc62e52f3be
JuhoL99 <juholauren99@gmail.com>
2024-10-17 14:20:26 +0300

    Assignment 2 5/5 completed
    Steps 1-5 working, code cleanup and organization, crane error logging

--- Assets/CraneAIControl.cs
+++ Assets/CraneAIControl.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class CraneAIControl : MonoBehaviour
 {
-    [SerializeField] private Transform craneForward;
     [SerializeField] private TowerCrane towerCrane;
     [SerializeField] private Trolley trolley;
     [SerializeField] private Cable cable;
@@ -12,43 +11,91 @@ public class CraneAIControl : MonoBehaviour
     [SerializeField] private Transform trolleyNearLimit;
     [SerializeField] private Transform trolleyFarLimit;
 
+    [SerializeField] private bool debugMode = false;
+    private bool craneIsMoving = false;
+
+    private Transform craneForward;
+    private Transform craneTransform;
+    private Transform trolleyTransform;
+    private Transform cableTransform;
+
+    private Vector2 concretePos2D;
+    private Vector2 cranePos2D;
+    private Vector2 craneForward2D;
+    private Vector2 trolleyPos2D;
+
+    private Vector2 concreteDirection;
+
+    private int gizmoCount = 1000;
+    private Vector3[] gizmoPositions;
+
+
+    private void Start()
+    {
+        craneForward = towerCrane.gameObject.transform.GetChild(0);
+        craneTransform = towerCrane.gameObject.transform;
+        trolleyTransform = trolley.gameObject.transform;
+        cableTransform = cable.gameObject.transform;
+    }
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && HitConcrete())
+        VectorCalculations();
+        if(Input.GetMouseButtonDown(0) && HitConcrete() && !craneIsMoving)
         {
             StartCoroutine(RotateTowardConcrete());
         }
     }
     private IEnumerator RotateTowardConcrete()
     {
+        float craneRotationError = 0;
+        float trolleyDistanceError = 0;
+        craneIsMoving = !craneIsMoving;
         while(!FacingConcrete())
         {
+            if (debugMode)
+                Debug.Log("crane moving");
             towerCrane.CraneRotation(GetRotationDirection(), true);
             yield return null;
-        }  
-        while(!TrolleyAboveConcrete())
+        }
+        craneRotationError = Mathf.Abs(Vector2.SignedAngle(craneForward2D, concreteDirection));
+        while (!TrolleyAboveConcrete())
         {
-            Debug.Log("trolley moving");
+            if(debugMode)
+                Debug.Log("trolley moving");
             trolley.TrolleyManualMove(GetTrolleyMoveDirection());
             yield return null;
         }
-        while(!HookInConcrete())
+        trolleyDistanceError = Vector2.Distance(trolleyPos2D, concretePos2D);
+        while (!HookInConcrete())
         {
-            Debug.Log("cable moving to concrete");
+            if(debugMode)
+                Debug.Log("cable moving to concrete");
             cable.CableManualMove(GetCableMoveDirection());
             yield return null;
         }
         yield return new WaitForSeconds(1f);
         while(!CableAtMaxHeight())
         {
-            Debug.Log("cable moving up");
+            if(debugMode)
+                Debug.Log("cable moving up");
             cable.CableManualMove(1);
             yield return null;
         }
         yield return new WaitForSeconds(1f);
         ResetConcrete();
+        craneIsMoving = !craneIsMoving;
+        Debug.Log($"Crane rotation error: {craneRotationError}, Trolley distance error: {trolleyDistanceError}");
         yield return null;
     }
+    private void VectorCalculations()
+    {
+        cranePos2D = new Vector2(craneTransform.position.x, craneTransform.position.z);
+        concretePos2D = new Vector2(concrete.position.x, concrete.position.z);
+        craneForward2D = new Vector2(craneForward.forward.x, craneForward.forward.z);
+        trolleyPos2D = new Vector2(trolleyTransform.position.x, trolleyTransform.position.z);
+        concreteDirection = (concretePos2D - cranePos2D).normalized;
+    }
+    //disconnect concrete and set random position
     private void ResetConcrete()
     {
         Concrete cScript = concrete.GetComponent<Concrete>();
@@ -57,23 +104,23 @@ public class CraneAIControl : MonoBehaviour
         hScript.Disconnect();
         concrete.position = GetRandomWorldPosition();
     }
-    private float GetRotationDirection()
+    //check for closest rotation direction to concrete
+    private int GetRotationDirection()
     {
-        Vector2 cranePos2D = new Vector2(towerCrane.gameObject.transform.position.x, towerCrane.gameObject.transform.position.z);
-        Vector2 concretePos2D = new Vector2(concrete.position.x, concrete.position.z);
-        Vector2 craneForward2D = new Vector2(craneForward.forward.x, craneForward.forward.z);
-        Vector2 concreteDirection = (concretePos2D - cranePos2D).normalized;
         float signedAngle = Vector2.SignedAngle(craneForward2D, concreteDirection);
         return (int)Mathf.Sign(-signedAngle);
     }
+    //cable is at max height when its y position is same as trolley
     private bool CableAtMaxHeight()
     {
-        return cable.gameObject.transform.position.y == trolley.transform.position.y;
+        return Mathf.Abs(cableTransform.position.y - trolleyTransform.position.y) < 0.01f;
     }
+    //check if hook and concrete are close enough
     private bool HookInConcrete()
     {
-        return Mathf.Abs((hook.position.y + hook.gameObject.GetComponent<BoxCollider>().center.y) - concrete.position.y) < 0.1f; //hook attach point is different from hook origin
+        return Mathf.Abs((hook.position.y + hook.gameObject.GetComponent<BoxCollider>().center.y) - concrete.position.y) < 0.1f;
     }
+    //check if concrete is above or below hook
     private int GetCableMoveDirection()
     {
         float hookY = hook.position.y + hook.gameObject.GetComponent<BoxCollider>().center.y;
@@ -82,42 +129,33 @@ public class CraneAIControl : MonoBehaviour
             return -1;
         return 1;
     }
+    //if distance between crane and concrete is greater than crane and trolley move pos
     private int GetTrolleyMoveDirection()
     {
-        //if distance between crane and concrete is greater than crane and trolley move pos
-        Transform trolleyT = trolley.gameObject.transform;
-        Vector2 concretePos2D = new Vector2(concrete.position.x, concrete.position.z);
-        Vector2 trolleyPos2D = new Vector2(trolleyT.position.x, trolleyT.position.z);
-        Vector2 cranePos2D = new Vector2(towerCrane.gameObject.transform.position.x, towerCrane.gameObject.transform.position.z);
         if (Vector2.Distance(cranePos2D, concretePos2D) > Vector2.Distance(cranePos2D, trolleyPos2D))
             return 1;
         return -1;
 
     }
+    //check if xz coordinates of trolley and concrete are close enough
     private bool TrolleyAboveConcrete()
     {
-        Transform trolleyT = trolley.gameObject.transform;
-        Vector2 trolleyPos2D = new Vector2(trolleyT.position.x, trolleyT.position.z);
-        Vector2 concretePos2D = new Vector2(concrete.position.x, concrete.position.z);
         return Vector2.Distance(trolleyPos2D, concretePos2D) < 0.1f;
     }
     private bool FacingConcrete()
     {
-        Vector2 cranePos2D = new Vector2(towerCrane.gameObject.transform.position.x, towerCrane.gameObject.transform.position.z);
-        Vector2 concretePos2D = new Vector2(concrete.position.x, concrete.position.z);
-        Vector2 craneForward2D = new Vector2(craneForward.forward.x, craneForward.forward.z);
-        Vector2 concreteDirection = (concretePos2D - cranePos2D).normalized;
         float dot = Vector2.Dot(concreteDirection, craneForward2D);
         return dot > 0.99999f;
     }
-    public Vector3 GetRandomWorldPosition()
+    //get random point within a hollow cylinder around the crane
+    private Vector3 GetRandomWorldPosition()
     {
-        Vector2 cranePos2D = new Vector2(towerCrane.gameObject.transform.position.x, towerCrane.gameObject.transform.position.z);
         Vector2 trolleyNearLimit2D = new Vector2(trolleyNearLimit.position.x, trolleyNearLimit.position.z);
         Vector2 trolleyFarLimit2D = new Vector2(trolleyFarLimit.position.x, trolleyFarLimit.position.z);
+        
         float innerRadius = Vector2.Distance(cranePos2D, trolleyNearLimit2D);
-        float outerRadius = Vector2.Distance(trolleyNearLimit2D, trolleyFarLimit2D);
-        outerRadius += innerRadius;
+        float outerRadius = Vector2.Distance(cranePos2D, trolleyFarLimit2D);
+
         float angle = Random.Range(0, Mathf.PI * 2);
         float radius = Random.Range(innerRadius, outerRadius);
 
@@ -127,6 +165,7 @@ public class CraneAIControl : MonoBehaviour
 
         return new Vector3(x,y,z);
     }
+    //check if cursor hits the concrete
     private bool HitConcrete()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -135,5 +174,17 @@ public class CraneAIControl : MonoBehaviour
             return hitInfo.collider.name == "Concrete";
         }
     }
-
+    //visualize random world point placement
+    private void OnDrawGizmos()
+    {
+        if (!debugMode || !Application.isPlaying)
+            return;
+        gizmoPositions = new Vector3[gizmoCount];
+        Gizmos.color = Color.red;
+        for (int i = 0; i < gizmoCount; i++)
+        {
+            gizmoPositions[i] = GetRandomWorldPosition();
+            Gizmos.DrawSphere(gizmoPositions[i], 0.5f);
+        }
+    }
 }
